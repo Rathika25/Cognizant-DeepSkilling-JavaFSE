bc2032d237ce4bf5fcee1441a992f038b82c51d1
Rathika25 <rathika369yadav@gmail.com>
2026-07-04 14:54:35 +0530

    JUnit Basic Testing Exercises

---
+++ WEEK - 1/Module 04 - TDD using JUnit5 and Mockito/1. JUnit Basic Testing Exercises/Exercise 4 - AAA Pattern/CalculatorAAATest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorAAATest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 10;
+        int b = 20;
+
+        int result = calculator.add(a, b);
+
+        assertEquals(30, result);
+    }
+}
