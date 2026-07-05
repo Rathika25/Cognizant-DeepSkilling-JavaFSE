21aad11ae07a59ed6bf64e36589977cf73cabab0
Rathika25 <rathika369yadav@gmail.com>
2026-07-05 22:40:14 +0530

    Completed REST service to fetch country details based on country code

--- WEEK - 3/Module 07/Spring rest hands-on exercises/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ WEEK - 3/Module 07/Spring rest hands-on exercises/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,23 +1,21 @@
 package com.cognizant.springlearn.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
 
 @RestController
 public class CountryController {
 
-    @RequestMapping("/country")
-    public Country getCountryIndia() {
+    @Autowired
+    private CountryService countryService;
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        return country;
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
     }
 }
